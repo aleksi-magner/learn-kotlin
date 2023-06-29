db8fd763faa27e74b6a8bb1e5cc49fdd281bf8ce
AleksiMagner <aleksey.magner@yahoo.com>
2023-06-29 10:42:50 +0300

    Добавлена конвертация двоичных чисел в десятичные

---
+++ src/main/kotlin/BinaryNumbers.kt
@@ -0,0 +1,24 @@
+import kotlin.math.pow
+
+fun main() {
+    println(convertingBinaryNumbersToDecimal("")) // 0
+    println(convertingBinaryNumbersToDecimal("100010")) // 34
+    println(convertingBinaryNumbersToDecimal("100110")) // 38
+    println(convertingBinaryNumbersToDecimal("100101")) // 37
+    println(convertingBinaryNumbersToDecimal("101101")) // 45
+    println(convertingBinaryNumbersToDecimal("01001101")) // 77
+}
+
+fun convertingBinaryNumbersToDecimal(number: String): Int {
+    val reverseNumber = number.reversed()
+
+    var result = 0
+
+    repeat(number.length) {
+        if (reverseNumber[it].digitToInt() > 0) {
+            result += 2.0.pow(it.toDouble()).toInt()
+        }
+    }
+
+    return result
+}
