c92d1d3d30ee540ffd685ebf15a68c62a83a4bc2
AleksiMagner <aleksey.magner@yahoo.com>
2023-07-05 12:15:19 +0300

    Добавлен раздел Gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,24 +1,39 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+group = "ru.magner"
+version = "1.0-SNAPSHOT"
+description = "Проект для обучения Kotlin"
+
 plugins {
     kotlin("jvm") version "1.8.22"
 }
-
-group = "ru.magner"
-version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
+    implementation("com.diogonunes:JCDP:4.0.2")
+
     testImplementation(kotlin("test"))
 }
 
-tasks.test {
-    useJUnitPlatform()
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(19))
+    }
 }
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "19"
+}
+
+tasks.register("helloGradle") {
+    doLast {
+        println("Hello, Gradle!")
+    }
+}
+
+tasks.test {
+    useJUnitPlatform()
 }
