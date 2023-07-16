8404940745d6cd2141d5fe81d2a50bb452af8d04
AleksiMagner <aleksey.magner@yahoo.com>
2023-07-16 10:23:47 +0300

    Расширен раздел Строки

--- src/main/kotlin/Strings.kt
+++ src/main/kotlin/Strings.kt
@@ -14,9 +14,9 @@ fun main() {
     val two = "2"
     val twelve = one + two
 
-    println(one)      // 1, no changes
-    println(two)      // 2, no changes
-    println(twelve)   // 12
+    println(one) // 1, no changes
+    println(two) // 2, no changes
+    println(twelve) // 12
 
     // Добавление значений
     val stringPlusBoolean = "abc" + 10 + true
@@ -107,4 +107,40 @@ fun main() {
     println(fullName)
 
     println("$language has ${language.length} letters in the name")
+
+    /**
+     * Получение символов строки
+     */
+    val greeting = "Hello"
+
+    val first: Char = greeting[0] // 'H'
+    val second: Char = greeting[1] // 'e'
+    val five: Char = greeting[4] // 'o'
+    val last: Char = greeting[greeting.length - 1] // 'o'
+    val preLast: Char = greeting[greeting.length - 2] // 'l'
+
+    println("First char: ${greeting.first()}") // 'H'
+    println("First char: $first") // 'H'
+    println("Second char: $second") // 'e'
+    println("Five char: $five") // 'o'
+    println("Last char: $last") // 'o'
+    println("Last char: ${greeting.last()}") // 'o'
+    println("Pre-last char: $preLast") // 'l'
+    println("Last index: ${greeting.lastIndex}") // 4
+
+    println("String is empty: ${greeting.isEmpty()}") // false
+    println("String is not empty: ${greeting.isNotEmpty()}") // true
+
+    personInformation()
+}
+
+/**
+ * Считывает из одной строки ввода имя, фамилию и возраст человека, а затем выводит информацию по шаблону
+ */
+fun personInformation() {
+    print("Введите через пробел имя, фамилию и возраст: ")
+
+    val (firstName, lastName, age) = readln().split(" ")
+
+    println("${firstName.first()}. $lastName, $age years old")
 }
