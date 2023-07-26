fdbe322f312799cfff10fe104e3e0d38637478ac
AleksiMagner <aleksey.magner@yahoo.com>
2023-07-26 21:59:01 +0300

    Расширен раздел Типы

--- src/main/kotlin/Types.kt
+++ src/main/kotlin/Types.kt
@@ -76,9 +76,145 @@ fun main() {
     // Strings
     val text: String = "Hello, I am studying Kotlin now."
     val creditCardNumber = "1234 5678 9012 3456"
+
+    typeCastAndSmartCast()
 }
 
 // Не возвращает управление, выполнение кода останавливается. Поэтому тип Nothing
 fun fail(): Nothing {
     throw Exception("Fail!")
 }
+
+/**
+ * Функция принимает аргумент типа Any, что означает, что она может принимать любой тип объекта.
+ *
+ * Внутри функции мы используем when с is для проверки типа входного объекта
+ */
+fun processInput(input: Any) {
+    when (input) {
+        is Int -> println("Input is an integer")
+        is String -> println("Input is a string")
+        is Double -> println("Input is a double")
+        else -> println("Unknown input")
+    }
+}
+
+/**
+ * Чтобы проверить, является ли объект экземпляром определённого параметра типа, мы можем использовать оператор is с параметром типа в угловых скобках.
+ */
+inline fun <reified T> exampleFunction1(obj: Any) {
+    if (obj is T) {
+        println("obj is an instance of type parameter T")
+    } else {
+        println("obj is not an instance of type parameter T")
+    }
+}
+
+/**
+ * Мы можем привести объект к параметру типа, используя оператор as с параметром типа в угловых скобках. Однако если объект не является экземпляром параметра типа, будет выброшено исключение ClassCastException.
+ *
+ * Чтобы избежать этого, мы можем использовать оператор безопасного приведения as?, который возвращает null, если приведение невозможно.
+ */
+fun <T> exampleFunction2(obj: Any) {
+    val tObj: T? = obj as? T
+
+    if (tObj != null) {
+        println("obj can be safely cast to type parameter T")
+    } else {
+        println("obj cannot be cast to type parameter T")
+    }
+}
+
+/**
+ * Проверка типов и приведение типов необходимы для любого языка программирования. Проверки типов позволяют разработчикам проверять, принадлежит ли объект к определённому типу данных, а приведения типов позволяют программистам преобразовывать объект из одного типа в другой.
+ *
+ * Kotlin, будучи языком со статической типизацией, имеет несколько функций, делающих проверку и приведение типов простыми и безопасными в использовании.
+ */
+fun typeCastAndSmartCast() {
+    println("--- is and !is operators ---")
+
+    /**
+     * Оператор is возвращает значение true, если объект принадлежит к указанному типу, и значение false, если нет. И наоборот, оператор !is возвращает true, если объект не принадлежит к указанному типу, и false, если принадлежит.
+     */
+    val obj: Any = "Hello, Kotlin"
+
+    if (obj is String) {
+        println(obj.uppercase()) // HELLO, KOTLIN
+    } else {
+        println("obj is not a String") // Не будет вызван, т.к. obj строка и проходит условие
+    }
+
+    processInput(42) // Input is an integer
+    processInput(obj) // Input is a string
+    processInput(3.14) // Input is a double
+    processInput(3.14f) // Unknown input
+
+    println("--- Smart casts ---")
+
+    /**
+     * В Kotlin также есть функция, известная как умные приведения (smart casts).
+     *
+     * Смарт-приведения используются для упрощения кода при работе с типами, допускающими значение null.
+     *
+     * Когда тип, допускающий значение null, проверяется с помощью оператора is, Kotlin автоматически приводит объект к типу, не допускающему значение null.
+     */
+    val any: Any? = null
+
+    if (any is String) {
+        println(any.length) // Не будет вызван
+    } else {
+        println("any is not a String") // Будет вызван
+    }
+
+    println("--- 'Unsafe' cast operator ---")
+
+    /**
+     * В Kotlin есть небезопасный оператор приведения, который представлен ключевым словом as. Ключевое слово as используется для приведения объекта к ненулевому типу. Если объект не может быть приведён к указанному типу, оператор as создаёт исключение ClassCastException
+     */
+    val obj2: Any = "Hello, Kotlin"
+
+    // Используем оператор as для приведения переменной obj2 к строке
+    val str2: String = obj2 as String // Unsafe cast operator
+
+    println(str2.uppercase()) // HELLO, KOTLIN
+
+    // NullPointerException: null cannot be cast to non-null type kotlin.String
+    // println(any as String)
+
+    println("--- 'Safe' (nullable) cast operator ---")
+
+    /**
+     * `as?` оператор используется для приведения объекта к типу, допускающему значение null. Если объект не может быть приведён к указанному типу, оператор `as?` возвращает null.
+     */
+    val obj3: Any = 123
+    val str3: String? = obj3 as? String // Safe (nullable) cast operator
+
+    if (str3 != null) {
+        println(str3.uppercase()) // Не будет вызван
+    }
+
+    println(any as? String) // null
+
+    println("--- Generics type checks and casts ---")
+
+    /**
+     * В Kotlin мы также можем использовать проверки типов и приведения типов с дженериками. При работе с дженериками нам может понадобиться проверить, является ли объект экземпляром определённого параметра типа или привести его к параметру типа.
+     *
+     * Чтобы проверить, является ли объект экземпляром определённого параметра типа, мы можем использовать оператор is с параметром типа в угловых скобках.
+     *
+     * Важно отметить, что в Kotlin с дженериками происходит стирание типа, а это означает, что фактический тип универсального объекта неизвестен во время выполнения. Поэтому некоторые операции, такие как создание нового экземпляра параметра типа или проверка того, является ли параметр типа подтипом другого класса, невозможны.
+     */
+
+    println("--- Tasks ---")
+
+    val list = listOf("abc", 42, '$', 3.14f, "string")
+
+    println(countStrings(list)) // 2
+}
+
+/**
+ * Принимает список объектов и возвращает количество строк в списке. Если элемент списка не является строкой, его необходимо пропустить.
+ *
+ * Функция должна использовать операторы is или !is для проверки типа элементов списка.
+ */
+fun countStrings(list: List<Any>): Int = list.count { it is String }
